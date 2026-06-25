3353fc999de7fd88a9608c9330f025460fcc5a26
alx-m24 <alexandremohamudally@gmail.com>
2026-06-25 16:39:12 +0800

    fresh start

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,5 @@
-import UI.InputHelper;
-import UI.Menu;
-import HouseHold.HouseHold;
-import java.util.Scanner;
-
-public class Main {
+class Main {
     public static void main(String[] args) {
-        Scanner scanner   = new Scanner(System.in);
-        InputHelper input = new InputHelper(scanner);
-        Menu menu         = new Menu(new HouseHold(), input);
-
-        int choice;
-        do {
-            menu.show();
-            choice = input.getInt("Enter choice: ");
-            menu.handleChoice(choice);
-        } while (choice != 9);
-
-        scanner.close();
+        System.out.println("Hello");
     }
 }
